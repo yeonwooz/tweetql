1111bfead0bada0b1458e8e2d5ba3b20dd76e608
suyeon woo <woosean999@gmail.com>
2023-05-28 21:33:17 +0900

    add. Non-Nullable 필드타입 정의 추가

--- server.js
+++ server.js
@@ -5,27 +5,29 @@ import {ApolloServer, gql} from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
-    parent: User
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
+    parent: User!
   }
   type Tweet {
     # Scalar필드를 가진 타입을 정의해주어야 한다(최소단위)
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
 
   # 필수타입 : Query
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]! # 유효한 Tweet리스트만 응답으로 받을 것
+    tweet(id: ID!): Tweet
   }
 
   # Post, Put, Patch, Delete 에 해당하는 데이터 변경 요청: Mutation
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
