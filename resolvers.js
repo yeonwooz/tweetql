a56ee72e2cbf87b913054cf4485e258da3e8ecad
suyeon woo <woosean999@gmail.com>
2023-05-28 22:10:22 +0900

    add. tweet 리졸버 추가

--- resolvers.js
+++ resolvers.js
@@ -10,6 +10,13 @@ const resolvers = {
     async allTweets() {
       return await getTweets();
     },
+    async tweet(root, args) {
+      // args는 유저가 보낸 패러미터
+      console.log(root, args);
+      const {id} = args;
+      const tweets = await getTweets();
+      return tweets.find(tweet => tweet.id === id);
+    },
   },
 };
 
